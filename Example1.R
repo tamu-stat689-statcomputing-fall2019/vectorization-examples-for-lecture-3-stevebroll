d3a9f48392431d555e766658d8c6a19a56a63996
stevebroll <stevebroll@tamu.edu>
2019-09-03 14:45:16 -0500

    add square_vec3; check identical; add microbenchmark

--- Example1.R
+++ Example1.R
@@ -24,12 +24,25 @@ square_vec2 <- function(x){
   x*x
 }
 
+square_vec3 <- function(x){
+  x**2
+}
 # [ToDo] Create a vector x of size 100,000 of normal variables
 
 x <- rnorm(100000)
 
 # [ToDo] Verify that all functions return the same output
+identical(square_for(x), square_sapply(x))
+identical(square_for(x), square_vec(x))
+identical(square_for(x), square_vec2(x))
 
 # [ToDo] Use microbenchmark package to compare three functions in terms of speed
 library(microbenchmark)
+
+microbenchmark(
+  square_for(x),
+  square_sapply(x),
+  square_vec(x),
+  square_vec2(x)
+)
 
